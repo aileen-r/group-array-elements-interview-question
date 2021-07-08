899589e478048765ed0079542f4d91f82b2e6042
Aileen Rae <aileenlrae@gmail.com>
2021-07-08 12:19:26 +0100

    Write groupArrayEls method + unit tests

--- src/groupArrayEls.js
+++ src/groupArrayEls.js
@@ -1,5 +1,19 @@
-const groupArrayElements = () => {
-    return [];
-}
+const groupArrayElements = (array, n) => {
+  if (!array || !n) {
+    throw "Both 'array' and 'n' arguments are required";
+  }
+  if (Number.isNaN(n) || !Number.isInteger(n) || n < 1) {
+    throw "n must be a positive integer";
+  }
+  if (!Array.isArray(array)) {
+    throw "'array' must be an array";
+  }
+  const result = [];
+  const groupSize = Math.ceil(array.length / n);
+    for (let i = 0; i < array.length; i += groupSize) {
+      result.push(array.slice(i, i + groupSize));
+    }
+  return result;
+};
 
 export default groupArrayElements;
--- test/groupArrayEls.test.js
+++ test/groupArrayEls.test.js
@@ -1,5 +1,47 @@
 import groupArrayElements from "../src/groupArrayEls";
 
-test("returns an empty array", () => {
-  expect(groupArrayElements()).toStrictEqual([]);
+const testInputArray = [1, 2, 3, 4, 5];
+const testOutputArray = [[1, 2], [3, 4], [5]];
+
+describe("input validation", () => {
+  test("throws if N is not a positive integer", () => {
+    expect(() => {
+      groupArrayElements(testInputArray, -1);
+    }).toThrow("n must be a positive integer");
+    expect(() => {
+      groupArrayElements(testInputArray, 2.5);
+    }).toThrow("n must be a positive integer");
+  });
+
+  test("throws if 'array' is not an array", () => {
+    expect(() => {
+      groupArrayElements("iAmNotAnArray", 4);
+    }).toThrow("'array' must be an array");
+  });
+
+  test("throws if any method arguements are missing", () => {
+    expect(() => {
+      groupArrayElements(testInputArray);
+    }).toThrow("Both 'array' and 'n' arguments are required");
+    expect(() => {
+      groupArrayElements();
+    }).toThrow("Both 'array' and 'n' arguments are required");
+  });
+});
+
+describe("expected outputs", () => {
+  test("returns empty array if array length is 0", () => {
+    expect(groupArrayElements([], 5)).toStrictEqual([]);
+  });
+  test("splits array according to example provided", () => {
+    expect(groupArrayElements(testInputArray, 3)).toStrictEqual(
+      testOutputArray
+    );
+  });
+  test("another split test", () => {
+    expect(groupArrayElements([1, 2, 3, 4, 5, 6], 2)).toStrictEqual([
+      [1, 2, 3],
+      [4, 5, 6],
+    ]);
+  });
 });
